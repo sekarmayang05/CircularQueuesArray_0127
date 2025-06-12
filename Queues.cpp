fd76c3db97d0ba841ebbdbe443878777f3ade04d
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-06-12 15:42:00 +0700

    jika REAR berada di posisi terakhir array, kembali ke awal array

--- Queues.cpp
+++ Queues.cpp
@@ -34,4 +34,13 @@ public:
             FRONT = 0; // 2.a
             REAR = 0;  //2.b
         }
-    }    
+        else
+        {
+            // jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max -1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
